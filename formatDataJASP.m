ed825b0e48eb17366ee2aa0fda265b554c854d93
biomusic <dannie.fu.888@gmail.com>
2021-01-15 16:09:53 -0500

    Adding HRV to JASP table

--- formatDataJASP.m
+++ formatDataJASP.m
@@ -40,16 +40,24 @@ HR_stop2 = [];
 HR_stop3 = [];
 HR_stop4= [];
 HR_barefoot= [];
+HRVZY_before = [];
+HRVZY_after = [];
+HRVZY_stop0 = [];
+HRVZY_stop1 = [];
+HRVZY_stop2 = [];
+HRVZY_stop3 = [];
+HRVZY_stop4= [];
+HRVZY_barefoot= [];
 
 % Init table with empty columns
-JASPtable = table(Participants,EDA_before,TEMP_before,HR_before, ... 
-    EDA_after,TEMP_after,HR_after, ...
-    EDA_stop0,TEMP_stop0,HR_stop0, ...
-    EDA_stop1,TEMP_stop1,HR_stop1, ...
-    EDA_stop2,TEMP_stop2,HR_stop2, ...
-    EDA_stop3,TEMP_stop3,HR_stop3, ...
-    EDA_stop4,TEMP_stop4,HR_stop4, ...
-    EDA_barefoot,TEMP_barefoot,HR_barefoot);
+JASPtable = table(Participants,EDA_before,TEMP_before,HR_before,HRVZY_before, ... 
+    EDA_after,TEMP_after,HR_after,HRVZY_after, ...
+    EDA_stop0,TEMP_stop0,HR_stop0,HRVZY_stop0, ...
+    EDA_stop1,TEMP_stop1,HR_stop1,HRVZY_stop1, ...
+    EDA_stop2,TEMP_stop2,HR_stop2,HRVZY_stop2, ...
+    EDA_stop3,TEMP_stop3,HR_stop3,HRVZY_stop3,...
+    EDA_stop4,TEMP_stop4,HR_stop4,HRVZY_stop4, ...
+    EDA_barefoot,TEMP_barefoot,HR_barefoot,HRVZY_barefoot);
 
 tableRow = [];
 
@@ -63,18 +71,23 @@ for k=1:length(subfolders)
     for i=1:length(sections)
         
         % Load the slopes for EDA, HR, TEMP in that window
-        load(strcat(LOAD_DIR,char(subfolders(k)),'/',char(sections(i)),'_slopes.mat')); 
+        slopes = load(strcat(LOAD_DIR,char(subfolders(k)),'/',char(sections(i)),'_slopes.mat')); 
+        aves = load(strcat(LOAD_DIR,char(subfolders(k)),'/',char(sections(i)),'_ave.mat')); 
+
 
         % Compute the standard deviation of the EDA slopes and add to
         % column
-        stdEDAslopes = std(EDA(:,2),'omitnan');
+        stdEDAslopes = std(slopes.EDA(:,2),'omitnan');
 
         % Compute the median of the HR and TEMP slopes and add to
         % column
-        medianHRslopes = median(HR(:,2),'omitnan');
-        medianTEMPslopes = median(TEMP(:,2),'omitnan');
+        medianHRslopes = median(slopes.HR(:,2),'omitnan');
+        medianTEMPslopes = median(slopes.TEMP(:,2),'omitnan');
+        
+        % Compute mean of HRVZY values and add to column
+        meanHRVZY = mean(aves.HRVZY(:,2),'omitnan');
 
-        tableRow = [tableRow,stdEDAslopes,medianTEMPslopes,medianHRslopes];
+        tableRow = [tableRow,stdEDAslopes,medianTEMPslopes,medianHRslopes,meanHRVZY];
     end
     
     JASPtable = [JASPtable; tableRow];
