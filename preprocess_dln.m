25eda432930139f648517dfd03d57b109ffbf127
biomusic <dannie.fu.888@gmail.com>
2021-01-22 11:00:41 -0500

    Making preprocessing script into function

--- preprocess_dln.m
+++ preprocess_dln.m
@@ -2,13 +2,14 @@
 
 clear;
 
-LOAD_DIR = "/Volumes/Seagate/danslaNature/2020-09-19/group2/018/";
+LOAD_DIR = "/Volumes/Seagate/danslaNature/data/2020-09-19/group1/008/";
+OUT_DIR = "/Volumes/Seagate/danslaNature/analysis/2020-09-19/group1/008/";
 
 D = dir(LOAD_DIR);
 subfolders = setdiff({D([D.isdir]).name},{'.','..'}); % list of subfolders of D
 
 if isempty(subfolders) % If no segments 
-    OUT_DIR = LOAD_DIR;
+    %OUT_DIR = LOAD_DIR;
     files = dir(strcat(LOAD_DIR,'*.mat'));
     
     for i=1:length(files)
@@ -16,12 +17,23 @@ if isempty(subfolders) % If no segments
         load(filepath);
     end
     
-    preprocess;
+    clean = preprocess(EDA,TEMP,HR,HRV);
+    
+    %Save cleaned data
+    save(strcat(OUT_DIR,'clean.mat'),'-struct','clean');
     
 else % If data is in parts
     
+    clean.EDA = [];
+    clean.TEMP = [];
+    clean.HR = [];
+    clean.HRVX = [];
+    clean.HRVY = [];
+    clean.HRVZ = [];
+    clean.HRVYZ = [];
+
     for i=1:length(subfolders)
-        OUT_DIR = strcat(LOAD_DIR,subfolders(i),'/');
+        %OUT_DIR = strcat(LOAD_DIR,subfolders(i),'/');
 
         files = dir(strcat(LOAD_DIR, subfolders(i),'/*.mat'));
 
@@ -30,6 +42,19 @@ else % If data is in parts
             load(filepath);
         end 
 
-        preprocess;
+        c = preprocess(EDA,TEMP,HR,HRV);
+        
+        nan = [NaN NaN];
+        clean.EDA = [clean.EDA; nan; c.EDA];
+        clean.TEMP = [clean.TEMP; nan; c.TEMP];
+        clean.HR = [clean.HR; nan; c.HR];
+        clean.HRVX = [clean.HRVX; nan; c.HRVX];
+        clean.HRVY = [clean.HRVY; nan; c.HRVY];
+        clean.HRVZ = [clean.HRVZ; nan; c.HRVZ];
+        clean.HRVYZ = [clean.HRVYZ; nan; c.HRVYZ];
+        
+        % Save cleaned data
+        save(strcat(OUT_DIR,'clean.mat'),'-struct','clean');
     end
+    
 end 
